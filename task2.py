97a6fa6fb432de9eebe00dce7baa54a6f908b6f7
fisent <lukzieniewicz@gmail.com>
2017-06-15 13:37:28 +0200

    task2

--- task2.py
+++ task2.py
@@ -1,3 +1,30 @@
-SPELL_BOOK = dict{"fe":1, "je":2, "jee":3, "ain":3, "dai":5, "ne":2, "ai":2 }
+import functools
+
+SPELL_BOOK = {"fe":1, "je":2, "jee":3, "ain":3, "dai":5, "ne":2, "ai":2 }
+
+
 
 def damage(spell):
+    def custom_comparator(x, y):
+        if SPELL_BOOK[x] < SPELL_BOOK[y]:
+            return 1
+        else:
+            return -1
+
+    sorted_keys = list(SPELL_BOOK.keys())
+    sorted_keys.sort(key=functools.cmp_to_key(custom_comparator))
+    print(sorted_keys)
+
+    dmg = 0
+    for key in sorted_keys:
+        index = spell.find(key)
+        while index >= 0:
+            to_print = "subspell: " + spell[index:index+len(key)]
+            to_print += " before: " + spell
+            spell = spell[:index] + spell[index+len(key):]
+            print(to_print + " after: " + spell)
+            dmg += SPELL_BOOK[key]
+            index = spell.find(key)
+    return dmg
+
+print(damage("fejee"))
