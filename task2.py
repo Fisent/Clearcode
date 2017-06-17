09af56d6fb2d8831bfc0159ae0a13084f4a5022d
fisent <lukzieniewicz@gmail.com>
2017-06-17 17:51:53 +0200

    double sorting fixed all existing tests

--- task2.py
+++ task2.py
@@ -19,6 +19,15 @@ def custom_comparator(x,y):
     else:
         return -1
 
+def custom_comparator2(x,y):
+    if len(x) > len(y):
+        if SPELL_BOOK[x] < SPELL_BOOK[y]:
+            return 1
+        else:
+            return -1
+    else:
+        return -1
+
 sorted_keys = list(SPELL_BOOK.keys())
 sorted_keys.sort(key=functools.cmp_to_key(custom_comparator))
 print(sorted_keys)
@@ -28,13 +37,22 @@ def is_correct(spell):
     stop = spell.rfind("ai")
     return start >=0 and stop >= 0 and start < stop
 
+def find_subspells(spell):
+    out = list()
+    for subspell in SPELL_BOOK:
+        while subspell in spell:
+            out.append(subspell)
+            index = spell.find(subspell)
+            spell = spell[:index] + spell[index+len(subspell):]
+    return out
+
 def trim(spell):
     start = spell.find("fe")
     stop = spell.rfind("ai")
     return spell[start + 2: stop]
 
 
-def damage(spell):
+def damage_inner(spell):
 
     if is_correct(spell):
         spell = trim(spell)
@@ -49,3 +67,9 @@ def damage(spell):
             dmg += SPELL_BOOK[key]
             index = spell.find(key)
     return max(dmg -len(spell), 0)
+
+def damage(spell):
+    dmg1 = damage_inner(spell)
+    sorted_keys.sort(key=functools.cmp_to_key(custom_comparator2))
+    dmg2 = damage_inner(spell)
+    return max(dmg1, dmg2)
--- test.py
+++ test.py
@@ -12,17 +12,21 @@ class Task2TestCase(unittest.TestCase):
         self.assertEqual(task2.damage("fexxxx"), 0)
         self.assertEquals(task2.damage("aaa"), 0)
         self.assertEquals(task2.damage('feeai'), 2)
-        #self.assertEquals(task2.damage('feaineain'), 7)
+        self.assertEquals(task2.damage('feaineain'), 7)
         self.assertEquals(task2.damage('jee'), 0)
         self.assertEquals(task2.damage('fdafafeajain)'), 1)
-        self.assertTrue(task2.damage('fexxxxxxxxxxai') == 0)
+        self.assertEquals(task2.damage('fexxxxxxxxxxai'), 0)
         self.assertEquals(task2.damage('fedaiai'), 8)
+        self.assertEquals(task2.damage('feainjeeai'),9)
         print("damage OK")
 
     def test_is_correct(self):
         self.assertEquals(task2.is_correct("xxxxxfejejeeaindaiyaiaixxxxxx"), True)
         self.assertTrue(task2.is_correct("feeai"))
         print("is correct OK")
+
+    def test_find_subspells(self):
+        self.assertEquals(task2.find_subspells("fefeai"), ["fe","fe","ai"])
 
 
 if __name__ == '__main__':
